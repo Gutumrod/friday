751f957937c8f12523500dc0c4fa8946d6fe408e
Gutumrod <titazmth@gmail.com>
2026-08-26 18:36:21 +0700

    feat: enable confirm-gated smart-home controls in API runtime

--- src/friday/api_launcher.py
+++ src/friday/api_launcher.py
@@ -1,12 +1,14 @@
 """Guarded ASGI entrypoint for the Friday API service."""
 from friday import core
 from friday.home_assistant_runtime import install_home_assistant_read_tools
+from friday.home_control_runtime import install_home_control_tools
 from friday.home_device_runtime import install_home_device_read_tools
 from friday.runtime_security import apply_runtime_security
 
 apply_runtime_security(core)
 ha_client = install_home_assistant_read_tools(core)
-install_home_device_read_tools(core, client=ha_client)
+home_registry = install_home_device_read_tools(core, client=ha_client)
+install_home_control_tools(core, client=ha_client, registry=home_registry)
 
 from friday.api import app  # noqa: E402
 
