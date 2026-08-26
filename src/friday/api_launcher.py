efb6b02371882401a464391c32d6a40dfa05421e
Gutumrod <titazmth@gmail.com>
2026-08-26 18:33:53 +0700

    feat: enable semantic home device registry in API runtime

--- src/friday/api_launcher.py
+++ src/friday/api_launcher.py
@@ -1,10 +1,12 @@
 """Guarded ASGI entrypoint for the Friday API service."""
 from friday import core
 from friday.home_assistant_runtime import install_home_assistant_read_tools
+from friday.home_device_runtime import install_home_device_read_tools
 from friday.runtime_security import apply_runtime_security
 
 apply_runtime_security(core)
-install_home_assistant_read_tools(core)
+ha_client = install_home_assistant_read_tools(core)
+install_home_device_read_tools(core, client=ha_client)
 
 from friday.api import app  # noqa: E402
 
