6c9e3d1473bcd94d01e738887f4fc9c7a42158f0
Gutumrod <titazmth@gmail.com>
2026-08-26 18:48:03 +0700

    feat: enable Home Assistant scene orchestration in API runtime

--- src/friday/api_launcher.py
+++ src/friday/api_launcher.py
@@ -3,12 +3,14 @@ from friday import core
 from friday.home_assistant_runtime import install_home_assistant_read_tools
 from friday.home_control_runtime import install_home_control_tools
 from friday.home_device_runtime import install_home_device_read_tools
+from friday.home_scene_runtime import install_home_scene_tools
 from friday.runtime_security import apply_runtime_security
 
 apply_runtime_security(core)
 ha_client = install_home_assistant_read_tools(core)
 home_registry = install_home_device_read_tools(core, client=ha_client)
 install_home_control_tools(core, client=ha_client, registry=home_registry)
+install_home_scene_tools(core, client=ha_client)
 
 from friday.api import app  # noqa: E402
 
